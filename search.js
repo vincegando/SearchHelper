53f1e7ff46248d85f9e79539062e14cfd3e6896a
Vince Gandolfo <vgandolfo@umail.ucsb.edu>
2016-08-30 19:01:30 -0700

    added functionality for Facebook, YouTube, and Twitter

--- search.js
+++ search.js
@@ -3,8 +3,41 @@ searchReddit = function(query) {
 	chrome.tabs.create({url : "https://www.reddit.com/search?q=" + searchQuery});
 };
 
+searchFacebook = function(query) {
+	var searchQuery = query.selectionText;
+	chrome.tabs.create({url : "https://www.facebook.com/search/top/?q=" + searchQuery});
+};
+
+searchYouTube = function(query) {
+	var searchQuery = query.selectionText;
+	chrome.tabs.create({url : "https://www.youtube.com/results?search_query=" + searchQuery});
+};
+
+searchTwitter = function(query) {
+	var searchQuery = query.selectionText;
+	chrome.tabs.create({url : "https://twitter.com/search?q=" + searchQuery});
+};
+
 chrome.contextMenus.create({
-	title : "Search Helper",
+	title : "Search Reddit",
 	contexts: ["selection"],
 	onclick: searchReddit
+});
+
+chrome.contextMenus.create({
+	title : "Search Facebook",
+	contexts: ["selection"],
+	onclick: searchFacebook
+});
+
+chrome.contextMenus.create({
+	title : "Search YouTube",
+	contexts: ["selection"],
+	onclick: searchYouTube
+});
+
+chrome.contextMenus.create({
+	title : "Search Twitter",
+	contexts: ["selection"],
+	onclick: searchTwitter
 });
